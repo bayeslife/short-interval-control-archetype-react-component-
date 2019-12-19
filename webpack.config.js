463df9da635bca2946aa3c2186cef2ad212e351d
P Tomlinson <ptomlinson@quartileone.com>
2019-12-19 21:08:44 +1000

    Fix issue

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
+const DEMO_SOURCE_DIR=process.env.DEMO_SOURCE_DIR || 'demo'
+console.log(`Demo Source Dir: ${DEMO_SOURCE_DIR}`)
+
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, "../../demo/src/index.html"),
+    template: path.join(__dirname, `../../${DEMO_SOURCE_DIR}/src/index.html`),
     filename: "./index.html",
     inlineSource: '.(js|css)$' // embed all javascript and css inline
 });
 const htmlWebpackInlineSourcePlugin = new HtmlWebpackInlineSourcePlugin()
 
-const DEMO_SOURCE_DIR=process.env.DEMO_SOURCE_DIR || 'demo'
-console.log(`Demo Source Dir: ${DEMO_SOURCE_DIR}`)
 module.exports = {
     entry: path.join(__dirname, `../../${DEMO_SOURCE_DIR}/src/index.js`),
     output: {
