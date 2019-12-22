58c83114e47fc809eb83253a4ee122419538572b
P Tomlinson <ptomlinson@quartileone.com>
2019-12-23 09:03:56 +1000

    Add demo-client-server functionality

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ To find out more about archetypes see [here](https://gitlab.com/short-interval-c
 
 ## To install
 
-```npm install short-interval-control-archetype-react-component short-interval-control-archetype-react-component-dev```
+```
+npm install --save short-interval-control-archetype-react-component 
+npm install --save-dev short-interval-control-archetype-react-component-dev
+```
 
 ## To use
-
 
 You can then define scripts which call out to any of the scripts in this module.
 For example:
@@ -21,6 +23,19 @@ For example:
     "test": "builder run archetype:test",
     "build": "builder run archetype:build",
     "lint": "builder run archetype:lint",
+    "demo": "DEMO_SOURCE_DIR=demo builder run archetype:demo",
+    "demo-client-server": "DEMO_SOURCE_DIR=demo-client-server builder run archetype:demo-client-server",
   },
   ```
+
+### For Demo Functionality
+
+Create an index.html which renders the component, then run ```npm run demo```
+
+### For Demo-Client-Server Functionality
+
+- Create an client/index.html which renders the component
+- Create a  server/index.js which provides the back end for the component and includes [webpack-dev-middleware](https://webpack.js.org/guides/development/#using-webpack-dev-middleware)
+- Then run ```npm run demo-client-server```
+
 
---
+++ webpack.client-server.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+const DEMO_SOURCE_DIR=process.env.DEMO_SOURCE_DIR || 'demo-client-server'
+console.log(`Demo Source Dir: ${DEMO_SOURCE_DIR}`)
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, `../../${DEMO_SOURCE_DIR}/client/index.html`),
+    filename: "./index.html",
+    inlineSource: '.(js|css)$' // embed all javascript and css inline
+});
+const htmlWebpackInlineSourcePlugin = new HtmlWebpackInlineSourcePlugin()
+
+module.exports = {
+    entry: path.join(__dirname, `../../${DEMO_SOURCE_DIR}/client/index.js`),
+    output: {
+        path: path.join(__dirname, `../../${DEMO_SOURCE_DIR}/dist`),
+        filename: "bundle.js",
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [htmlWebpackPlugin,htmlWebpackInlineSourcePlugin],
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+};
+
+
